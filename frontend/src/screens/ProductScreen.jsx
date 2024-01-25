cabfecd37c1ad07aee0703dc3ba024e8bc533ae1
ChrisHesse0 <125922062+ChrisHesse0@users.noreply.github.com>
2024-01-25 00:45:41 -0500

    Adds loader and error messages

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const ProductScreen = () => {
     const { id: productId } = useParams();
@@ -16,7 +18,7 @@ const ProductScreen = () => {
       Go Back
     </Link>
 
-    { isLoading ? (<h2>Loading...</h2>) : error ? (<div>{error?.data?.message || error.error}</div>) : (
+    { isLoading ? (<Loader />) : error ? (<Message variant='danger'>{error?.data.message || error.error}</Message>) : (
       <Row>
       <Col md={5}>
         <Image src={product.image} alt={product.name} fluid />
