8dc9773e2df538959e68ca52e0315dc762f7083d
ChrisHesse0 <125922062+ChrisHesse0@users.noreply.github.com>
2024-02-08 01:30:31 -0500

    Adds auth middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import productRoutes from "./routes/productRoutes.js"
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 5001;
 
@@ -22,6 +23,8 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 
 app.use('/api/users', userRoutes);
+
+app.use(cookieParser());
 
 app.use(notFound)
 app.use(errorHandler)
