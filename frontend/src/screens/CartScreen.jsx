0218a53d783fa9581621721f74223137772b3bf8
ChrisHesse0 <125922062+ChrisHesse0@users.noreply.github.com>
2024-02-16 21:07:03 -0500

    Bug fix with proper item count

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -79,7 +79,7 @@ const CartScreen = () => {
             <ListGroup variant='flush'>
               <ListGroup.Item>
                 <h2>
-                  Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                  Subtotal ({cartItems.reduce((acc, item) => acc + +item.qty, 0)})
                   items
                 </h2>
                 $
