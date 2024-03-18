d33d5db04cac807667fc93823423950bf4298f8d
ChrisHesse0 <125922062+ChrisHesse0@users.noreply.github.com>
2024-03-17 23:21:21 -0400

    Adds reviews in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { getProducts, getProductsById, createProduct, updateProduct, deleteProduct, } from "../controllers/productController.js";
+import { getProducts, getProductsById, createProduct, updateProduct, deleteProduct, createProductReview,} from "../controllers/productController.js";
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 
@@ -8,4 +8,5 @@ router.route('/').get(getProducts).post(protect, admin, createProduct);
 
 router.route('/:id').get(getProductsById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);;
 
+router.route('/:id/reviews').post(protect, createProductReview);
 export default router;
